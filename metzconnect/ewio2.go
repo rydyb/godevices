5f58901e30ac767c3e7fa949e441a8187df4bb14
Sascha Benz <93707849+SaBeBen@users.noreply.github.com>
2024-06-10 10:44:01 +0200

    adds leybold module, rewrites metzconnect module with native extension support (#4)
    * Introduce leybold dombivac cm52 driver
    
    * renamed folder according to project structure
    
    * Documentation, consistency in error messages and formatting
    
    * Consistent error messaging syntax, formatting
    
    * EWIO2 address has to be read differently
    
    * Extended functionality: identifying extensions and units, more verbose testing
    
    * rewrote the analog input handling to be more generic and extended tests
    
    * updated metzconnect package to embed analog inputs in EWIO2 and MRAI8
    
    * Switched to raw inputs for more precision, added MRCI4 and tests, basic structure for converting the units of the values
    
    * removed scaling of values in Value (metzconnect)
    
    ---------
    
    Co-authored-by: Bodo Kaiser <bodo.kaiser@physik.uni-muenchen.de>

--- metzconnect/ewio2.go
+++ metzconnect/ewio2.go
@@ -1,37 +1,129 @@
 package metzconnect
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
 
 	"github.com/goburrow/modbus"
 )
 
 type EWIO2 struct {
-	modbus.Client
+	analogInput
+	client modbus.Client
 }
 
-func (d *EWIO2) AnalogInput(port int) (float64, error) {
-	if port < 1 || port > 3 {
-		return 0.0, fmt.Errorf("EWIO2 has only analog inputs 1, 2, 3 not %d", port)
+func NewEWIO2(client modbus.Client) *EWIO2 {
+	return &EWIO2{
+		client: client,
+		analogInput: analogInput{
+			client:       client,
+			LowerChannel: 1,
+			UpperChannel: 3,
+			modeOffset:   60,
+			valueOffset:  70,
+		},
 	}
+}
 
-	bytes, err := d.Client.ReadInputRegisters(0x46+uint16(port-1), 2)
+func (d *EWIO2) Version() (int, error) {
+	response, err := d.client.ReadHoldingRegisters(2, 1)
 	if err != nil {
-		return 0.0, fmt.Errorf("failed to read from modbus: %s", err)
+		return -1, fmt.Errorf("failed to read version via modbus: %s", err)
 	}
-
-	return readFloat16(bytes)
+	return int(response[1]), nil
 }
 
-func readFloat16(b []byte) (float64, error) {
-	buffer := bytes.NewBuffer(b)
+type ExtensionType uint8
 
-	var value float32
-	if err := binary.Read(buffer, binary.BigEndian, &value); err != nil {
-		return 0.0, err
+const (
+	ExtensionUnknown ExtensionType = iota
+	ExtensionMR_DO4
+	ExtensionMR_TO4
+	ExtensionMR_DI4
+	ExtensionMR_DI10
+	ExtensionMR_SI4
+	ExtensionMR_DIO4_2
+	ExtensionMR_AO4
+	ExtensionMR_AOP4
+	ExtensionMR_AI8
+	ExtensionMR_CI4
+)
+
+type Extension interface {
+	ID() int
+	Type() ExtensionType
+}
+
+func (d *EWIO2) Extensions() ([]Extension, error) {
+	extensions := []Extension{}
+
+	for i := 1; i < 8; i++ {
+		response, err := d.client.ReadHoldingRegisters(uint16(i*100), 1)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read extension type via modbus: %s", err)
+		}
+
+		extension := ExtensionType(response[1])
+		switch extension {
+		case ExtensionUnknown:
+			continue
+		case ExtensionMR_AI8:
+			extensions = append(extensions, NewMR8AI(d.client, i))
+		default:
+			return nil, fmt.Errorf("unsupported extension type: %d", extension)
+		}
 	}
 
-	return float64(value), nil
+	return extensions, nil
+}
+
+type MRAI8 struct {
+	analogInput
+	id int
+}
+
+func NewMR8AI(client modbus.Client, id int) *MRAI8 {
+	return &MRAI8{
+		id: id,
+		analogInput: analogInput{
+			client:       client,
+			LowerChannel: 1,
+			UpperChannel: 8,
+			modeOffset:   uint16(60 + 100*id),
+			valueOffset:  uint16(70 + 100*id),
+		},
+	}
+}
+
+func (d *MRAI8) ID() int {
+	return d.id
+}
+
+func (d *MRAI8) Type() ExtensionType {
+	return ExtensionMR_AI8
+}
+
+type MRCI4 struct {
+	analogInput
+	id int
+}
+
+func NewMRCI4(client modbus.Client, id int) *MRCI4 {
+	return &MRCI4{
+		id: id,
+		analogInput: analogInput{
+			client:       client,
+			LowerChannel: 1,
+			UpperChannel: 4,
+			modeOffset:   uint16(60 + 100*id),
+			valueOffset:  uint16(70 + 100*id),
+		},
+	}
+}
+
+func (d *MRCI4) ID() int {
+	return d.id
+}
+
+func (d *MRCI4) Type() ExtensionType {
+	return ExtensionMR_CI4
 }
--- metzconnect/ewio2_test.go
+++ metzconnect/ewio2_test.go
@@ -1,6 +1,7 @@
 package metzconnect
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -9,17 +10,108 @@ import (
 
 var address = os.Getenv("ADDRESS")
 
-func TestEWIO2_AnalogInput(t *testing.T) {
-	ewio2 := EWIO2{modbus.TCPClient(address)}
+var ewio2 = NewEWIO2(modbus.TCPClient(address))
 
-	for i := 1; i <= 3; i++ {
-		voltage, err := ewio2.AnalogInput(i)
-		if err != nil {
-			t.Fatalf("failed to read analog input: %s", err)
-		}
+func TestEWIO2_Version(t *testing.T) {
+	version, err := ewio2.Version()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Version: %d", version)
+}
 
-		if voltage < 0.0 || voltage > 10.0 {
-			t.Errorf("voltage out of range: %f", voltage)
+func TestEWIO2_AnalogInputs_Mode(t *testing.T) {
+	for i := 1; i < 4; i++ {
+		t.Run(fmt.Sprintf("E%d", i), func(t *testing.T) {
+			mode, err := ewio2.Mode(uint8(i))
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Logf("Mode: %d", mode)
+		})
+	}
+}
+
+func TestEWIO2_AnalogInputs_Unit(t *testing.T) {
+	for i := 1; i < 4; i++ {
+		t.Run(fmt.Sprintf("E%d", i), func(t *testing.T) {
+			unit, err := ewio2.Unit(uint8(i))
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Logf("Unit: %s", unit)
+		})
+	}
+}
+
+func TestEWIO2_AnalogInputs_Value(t *testing.T) {
+	for i := 1; i < 4; i++ {
+		t.Run(fmt.Sprintf("E%d", i), func(t *testing.T) {
+			value, err := ewio2.Value(uint8(i))
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Logf("Value: %v", value)
+		})
+	}
+}
+
+func TestEWIO2_Extensions(t *testing.T) {
+	extensions, err := ewio2.Extensions()
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, extension := range extensions {
+		t.Logf("Extension: %d %v", extension.ID(), extension.Type())
+
+		switch extension.Type() {
+		case ExtensionMR_AI8:
+			t.Run("MR-AI8", func(t *testing.T) {
+				mrai8, ok := extension.(*MRAI8)
+				if !ok {
+					t.Fatal("invalid extension type")
+				}
+
+				for i := 1; i < 9; i++ {
+					t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+						mode, err := mrai8.Mode(uint8(i))
+						if err != nil {
+							t.Fatal(err)
+						}
+
+						value, err := mrai8.Value(uint8(i))
+						if err != nil {
+							t.Fatal(err)
+						}
+
+						t.Logf("Value: %v, Mode: %d", value, mode)
+					})
+				}
+				
+			})
+		case ExtensionMR_CI4:
+			t.Run("MR-CI4", func(t *testing.T) {
+				mrci4, ok := extension.(*MRCI4)
+				if !ok {
+					t.Fatal("invalid extension type")
+				}
+
+				for i := 1; i < 5; i++ {
+					t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+						mode, err := mrci4.Mode(uint8(i))
+						if err != nil {
+							t.Fatal(err)
+						}
+
+						value, err := mrci4.Value(uint8(i))
+						if err != nil {
+							t.Fatal(err)
+						}
+
+						t.Logf("Value: %v, Mode: %d", value, mode)
+					})
+				}
+			})
 		}
 	}
 }
